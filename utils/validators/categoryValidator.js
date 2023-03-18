9e52b4f0f1120906e60bcab7902e8724166d2600
Doaa Abdel Hadi <102731688+Ali112001@users.noreply.github.com>
2023-03-18 17:29:54 +0200

    Update categoryValidator.js

--- utils/validators/categoryValidator.js
+++ utils/validators/categoryValidator.js
@@ -1,39 +1,28 @@
-const slugify = require('slugify');
-const { check, body } = require('express-validator');
-const validatorMiddleware = require('../../middlewares/validatorMiddleware');
+const { check } = require('express-validator');
+const validatorMiddleware = require("../../middlewares/validatorMiddleware")
 
 exports.getCategoryValidator = [
-  check('id').isMongoId().withMessage('Invalid category id format'),
-  validatorMiddleware,
+    check('id').isMongoId().withMessage("Invalid category id format"),
+    validatorMiddleware,
 ];
 
 exports.createCategoryValidator = [
-  check('name')
-    .notEmpty()
-    .withMessage('Category required')
-    .isLength({ min: 3 })
-    .withMessage('Too short category name')
-    .isLength({ max: 32 })
-    .withMessage('Too long category name')
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
-  validatorMiddleware,
+    check("name")
+        .notEmpty()
+        .withMessage("Category required")
+        .isLength({ min: 3})
+        .withMessage("Too short category name")
+        .isLength({ max: 32})
+        .withMessage("Too long category name"),
+    validatorMiddleware,
 ];
 
 exports.updateCategoryValidator = [
-  check('id').isMongoId().withMessage('Invalid category id format'),
-  body('name')
-    .optional()
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
-  validatorMiddleware,
+    check('id').isMongoId().withMessage("Invalid category id format"),
+    validatorMiddleware,
 ];
 
 exports.deleteCategoryValidator = [
-  check('id').isMongoId().withMessage('Invalid category id format'),
-  validatorMiddleware,
+    check('id').isMongoId().withMessage("Invalid category id format"),
+    validatorMiddleware,
 ];
