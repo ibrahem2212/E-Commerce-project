9f2108e331fa71188521580f9bd31d4f41d11d47
Doaa Abdel Hadi <102731688+Ali112001@users.noreply.github.com>
2023-03-18 17:08:42 +0200

    Update subcategoryModel.js

--- models/subcategoryModel.js
+++ models/subcategoryModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-const subCategoryschema = new mongoose.Schema(
+const subCategorySchema = new mongoose.Schema(
   {
     name: {
       type: String,
       trim: true,
       unique: [true, "SubCategory must be unique"],
-      minlength: [2, "Too short SubCategory name"],
-      maxlength: [32, "Too long SubCategory name"],
+      minlength: [2, "To short SubCategory name"],
+      maxlength: [32, "To long SubCategory name"],
     },
     slug: {
       type: String,
@@ -15,11 +15,11 @@ const subCategoryschema = new mongoose.Schema(
     },
     category: {
       type: mongoose.Schema.ObjectId,
-      ref: "category",
+      ref: "Category",
       required: [true, "SubCategory must be belong to parent category"],
     },
   },
-
   { timestamps: true }
 );
-module.exports = mongoose.model("subcategory", subCategoryschema);
+
+module.exports = mongoose.model("SubCategory", subCategorySchema);
