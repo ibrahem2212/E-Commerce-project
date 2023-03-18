296e1bc0b488df266b21fcc715cfc6ae46d689c1
Doaa Abdel Hadi <102731688+Ali112001@users.noreply.github.com>
2023-03-18 17:31:52 +0200

    Update productValidator.js

--- utils/validators/productValidator.js
+++ utils/validators/productValidator.js
@@ -1,19 +1,14 @@
-const slugify = require("slugify");
-const { check, body } = require("express-validator");
+const { check } = require("express-validator");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
-const Category = require("../../models/categorymodel");
-const subcategory = require("../../models/subcategoryModel");
+const Category = require("../../models/categoryModel");
+const SubCategory = require("../../models/subCategoryModel");
 
 exports.createProductValidator = [
   check("title")
     .isLength({ min: 3 })
     .withMessage("must be at least 3 chars")
     .notEmpty()
-    .withMessage("Product required")
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
+    .withMessage("Product required"),
   check("description")
     .notEmpty()
     .withMessage("Product description is required")
@@ -46,7 +41,6 @@ exports.createProductValidator = [
       }
       return true;
     }),
-
   check("colors")
     .optional()
     .isArray()
@@ -70,36 +64,38 @@ exports.createProductValidator = [
         }
       })
     ),
+
   check("subcategories")
     .optional()
     .isMongoId()
     .withMessage("Invalid ID formate")
     .custom((subcategoriesIds) =>
-      subcategory
-        .find({ _id: { $exists: true, $in: subcategoriesIds } }).then(
-          (result) => {
+      SubCategory.find({ _id: { $exists: true, $in: subcategoriesIds } }).then(
+        (result) => {
+          //Length result equal length of subcategories in body
           if (result.length < 1 || result.length !== subcategoriesIds.length) {
             return Promise.reject(new Error(`Invalid subcategories Ids`));
           }
-        })
+        }
+      )
+    )
+    .custom((val, { req }) =>
+      SubCategory.find({ category: req.body.category }).then(
+        (subcategories) => {
+          const subCategoriesIdsInDB = [];
+          subcategories.forEach((subCategory) => {
+            subCategoriesIdsInDB.push(subCategory._id.toString());
+          });
+          //check if subcategories ids in db include to subcategories in req.body(true/false)
+          const checker = (target, arr) => target.every((v) => arr.includes(v));
+          if (!checker(val, subCategoriesIdsInDB)) {
+            return Promise.reject(
+              new Error(`subcategories not belong to category`)
+            );
+          }
+        }
+      )
     ),
-  // .custom((val, { req }) =>
-  //   SubCategory.find({ category: req.body.category }).then(
-  //     (subcategories) => {
-  //       const subCategoriesIdsInDB = [];
-  //       subcategories.forEach((subCategory) => {
-  //         subCategoriesIdsInDB.push(subCategory._id.toString());
-  //       });
-  //       // check if subcategories ids in db include subcategories in req.body (true)
-  //       const checker = (target, arr) => target.every((v) => arr.includes(v));
-  //       if (!checker(val, subCategoriesIdsInDB)) {
-  //         return Promise.reject(
-  //           new Error(`subcategories not belong to category`)
-  //         );
-  //       }
-  //     }
-  //   )
-  // )
   check("brand").optional().isMongoId().withMessage("Invalid ID formate"),
   check("ratingsAverage")
     .optional()
@@ -113,7 +109,6 @@ exports.createProductValidator = [
     .optional()
     .isNumeric()
     .withMessage("ratingsQuantity must be a number"),
-
   validatorMiddleware,
 ];
 
@@ -124,12 +119,6 @@ exports.getProductValidator = [
 
 exports.updateProductValidator = [
   check("id").isMongoId().withMessage("Invalid ID formate"),
-  body("title")
-    .optional()
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
   validatorMiddleware,
 ];
 
