adeb183f6a44ba4aab7f1504d849233e1a128197
Doaa Abdel Hadi <102731688+Ali112001@users.noreply.github.com>
2023-03-18 17:28:56 +0200

    Update brandValidator.js

--- utils/validators/brandValidator.js
+++ utils/validators/brandValidator.js
@@ -1,9 +1,8 @@
-const slugify = require("slugify");
-const { check, body } = require("express-validator");
+const { check } = require("express-validator");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
 
 exports.getBrandValidator = [
-  check("id").isMongoId().withMessage("Invalid category id format"),
+  check("id").isMongoId().withMessage("Invalid Brand id format"),
   validatorMiddleware,
 ];
 
@@ -14,22 +13,12 @@ exports.createBrandValidator = [
     .isLength({ min: 3 })
     .withMessage("Too short Brand name")
     .isLength({ max: 32 })
-    .withMessage("Too long Brand name")
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
+    .withMessage("Too long Brand name"),
   validatorMiddleware,
 ];
 
 exports.updateBrandValidator = [
   check("id").isMongoId().withMessage("Invalid Brand id format"),
-  body("name")
-    .optional()
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
   validatorMiddleware,
 ];
 
