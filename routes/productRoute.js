02d8015162396520d33aa343cf2007fceb17cb98
ibrahem2212 <78043357+ibrahem2212@users.noreply.github.com>
2023-04-26 18:02:35 +0200

    Add files via upload

---
+++ routes/productRoute.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const {
+  getProductValidator,
+  createProductValidator,
+  updateProductValidator,
+  deleteProductValidator,
+} = require("../utils/validators/productValidator");
+
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  uploadProductImages,
+  resizeProductImages,
+} = require("../services/productService");
+const authService = require("../services/authService");
+const reviewRoute = require("./reviewRoute");
+
+const router = express.Router();
+router.use("/:productId/reviews", reviewRoute);
+
+router
+  .route("/")
+  .get(getProducts)
+  .post(
+    authService.protect,
+    authService.allowedTo("admin", "manager"),
+    uploadProductImages,
+    resizeProductImages,
+    createProductValidator,
+    createProduct
+  );
+router
+  .route("/:id")
+  .get(getProductValidator, getProduct)
+  .put(
+    authService.protect,
+    authService.allowedTo("admin", "manager"),
+    uploadProductImages,
+    resizeProductImages,
+    updateProductValidator,
+    updateProduct
+  )
+  .delete(
+    authService.protect,
+    authService.allowedTo("admin"),
+    deleteProductValidator,
+    deleteProduct
+  );
+
+module.exports = router;
