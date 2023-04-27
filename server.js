a146c5b1f1dbe5f111c3ca54bc93071e64e42cd0
unknown <doaahadyhady@gmail.com>
2023-04-27 23:12:50 +0200

    commit 25

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
 const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 dotenv.config({ path: "config.env" });
 const ApiError = require("./utils/apiError");
@@ -19,10 +21,15 @@ dbConnection();
 
 // express app
 const app = express();
+
+// Enable other domains to access your application
 app.use(cors());
 app.options("*", cors());
+
+// Compress all responses
 app.use(compression());
 
+// Checkout webhook
 app.post(
   "/webhook-checkout",
   express.raw({ type: "application/json" }),
@@ -30,13 +37,37 @@ app.post(
 );
 
 // Middlewares
-app.use(express.json());
+app.use(express.json({ limit: "20kb" }));
 app.use(express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
+
+// Limit each IP to 100 requests per `window` (here, per 15 minutes)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  message:
+    "Too many accounts created from this IP, please try again after 15min",
+});
+
+// Apply the rate limiting middleware to all requests
+app.use("/api", limiter);
+
+// Middleware to protect against HTTP Parameter Pollution attacks
+app.use(
+  hpp({
+    whitelist: [
+      "price",
+      "sold",
+      "quantity",
+      "ratingsQuantity",
+      "ratingsAverage",
+    ],
+  })
+);
 
 // Mount Routes
 mountRoutes(app);
