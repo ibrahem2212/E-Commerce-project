6f361a7d82e97c8fc9d341f217f8bb16415c950b
ibrahem2212 <ibrahemmahmoud@gmail.com>
2023-04-28 22:46:24 +0300

    commit 27

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,21 +1,40 @@
 const nodemailer = require("nodemailer");
 
 //Nodemailer
+
 const sendEmail = async (options) => {
   // 1) Create transporter ( service that will send email like "gmail","Mailgun" ,"mailtrap")
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.sendgrid.net",
+  //   port: 587,
+  //   auth: {
+  //     user: "apikey",
+  //     pass: process.env.SENDGRID_API_KEY,
+  //   },
+  //   tls: {
+  //     rejectUnauthorized: false,
+  //   },
+  // });
+
+  /////////////////////////////////
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT, // if secure false port = 587, if true port= 465
-    secure: true,
+    service: "Outlook365",
+    host: "smtp.office365.com",
+    port: "587",
+    tls: {
+      ciphers: "SSLv3",
+      rejectUnauthorized: false,
+    },
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     },
   });
 
+  /////////////////////////////////
   // 2) Define email options (like from, to, subject, email content)
   const mailOpts = {
-    from: "E-shop App <ibrahemmahmoud22122001@gmail.com>",
+    from: "E-shop App <ibrahem22122001@outlook.com>",
     to: options.email,
     subject: options.subject,
     text: options.message,
