7712b7e7b5b0995b25b84a306df580a2d88168c3
ibrahem2212 <ibrahemmahmoud@gmail.com>
2023-06-21 01:31:23 +0300

    commit 30

--- routes/recommendRoute.js
+++ routes/recommendRoute.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
-const {getLoggedUserData} =require('../services/userService');
+const { getLoggedUserData } = require("../services/userService");
 
 const { getRecommend } = require("../services/recommendService");
 const authService = require("../services/authService");
 
 const router = express.Router();
 
-router.route("/").get(authService.protect,getLoggedUserData,getRecommend);
+router.route("/").get(getRecommend);
 
 module.exports = router;
